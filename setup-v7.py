58486be6275ec290014f516feffc598e258cba69
001qez <001qez@gmail.com>
2016-02-27 01:36:15 +0800

    Added files via upload

---
+++ setup-v7.py
@@ -0,0 +1,19 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os"],
+                     "excludes": ["tkinter"],
+                     "include_files": ["products_details.json",
+                                       "powerpoint-json-v7.py",
+                                       "vba.txt"]}
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = 'Console'
+
+setup(  name = "powerpoint-json",
+        version = "0.7",
+        description = "powerpoint-json",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("powerpoint-json.py", base=base)])
